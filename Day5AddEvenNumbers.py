4f9e5f84a283af116b5df2c16e685be31b9c3c37
Aforlinda <150390443+Aforlinda@users.noreply.github.com>
2023-12-02 18:29:38 +0100

    Update Day5AddEvenNumbers.py

--- Day5AddEvenNumbers.py
+++ Day5AddEvenNumbers.py
@@ -1,16 +1,4 @@
-# target = int(input("Enter a number between 0 and 1000 ")) # Enter a number between 0 and 1000
-# # 🚨 Do not change the code above ☝️
-
-# # Write your code here 👇
-
-
-# the_sum = 0
-
-# for n in range(2, (target + 1), 2):
-#     the_sum += n
-# print(the_sum)
-
-target = int(input("Enter a number between 0 and 1000 ")) # Enter a number between 0 and 1000
+target = int(input("Enter a number between 0 and 1000\n ")) 
 # 🚨 Do not change the code above ☝️
 
 # Write your code here 👇
@@ -18,8 +6,6 @@ target = int(input("Enter a number between 0 and 1000 ")) # Enter a number betwe
 
 the_sum = 0
 
-for n in range(2, (target + 1)):
-    if n % 2 == 0:
-        the_sum += n
+for n in range(2, (target + 1), 2):
+    the_sum += n
 print(the_sum)
-
